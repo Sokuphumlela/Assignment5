8392d35ed68ea855c7c92644fd0278b9c86985bd
Sokuphumlela <nombila.s@gmail.com>
2019-05-22 04:28:32 +0200

    Assignment 8 and 9

---
+++ src/main/java/cput/Repository/Admin/Impl/BarberRepositoryImpl.java
@@ -0,0 +1,61 @@
+package cput.Repository.Admin.Impl;
+
+import cput.Domain.Admin.Barber;
+import cput.Repository.Admin.BarberRepository;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class BarberRepositoryImpl implements BarberRepository {
+
+    private static BarberRepositoryImpl repository = null;
+    private Set<Barber> barbers;
+
+    private BarberRepositoryImpl(){
+        this.barbers = new HashSet<>();
+    }
+
+    private Barber findBarber(String barberId) {
+
+        return this.barber.stream()
+                .filter(course -> course.getBarberId().trim().equals(barberId))
+                .findAny()
+                .orElse(null);
+    }
+
+    public static BarberRepositoryImpl getRepository(){
+        if (repository == null) repository = new BarberRepositoryImpl();
+        return repository;
+    }
+
+
+    public Barber create(Barber barber){
+        this.barbers.add(barber);
+        return barber;
+    }
+
+    public Barber read(final String barberId){
+        Barber barber = findBarber(barberId);
+        return barber;
+    }
+
+    public void delete(String barberId) {
+        Barber barber = findBarber(barberId);
+        if (barber != null) this.barbers.remove(barber);
+    }
+
+    public Barber update(Barber barber){
+        Barber toDelete = findBarber(barber.getBarberId());
+        if(toDelete != null) {
+            this.barbers.remove(toDelete);
+            return create(barber);
+        }
+        return null;
+    }
+
+
+    public Set<Barber> getAll(){
+        return this.barbers;
+    }
+}
+
